c9cd90c27a905a784f940775689cf3f53b13473a
akarshb23 <akarshb23@gmail.com>
2026-06-02 11:08:10 +0530

    file upload handling and deploying it on cloud

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+// multer is used to handle file uploads , we'll store it in temp folders and upload in cloud then delete
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+// a cloud service like aws for image/video uploads
+import { v2 as cloudinary } from 'cloudinary'
+// fs - file system for file handling , comes with node no need to install
+import fs from 'fs'
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null ;
+        const response =  await cloudinary.uploader.upload(localFilePath , {
+            resource_type:"auto"
+        })
+        fs.unlinkSync(localFilePath);
+        return response ;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null ;
+    }
+}
+
+export {uploadCloudinary}
